9689da124345cb63fd400c86130c8bcab5ed5337
SAIRU <81401104+Sairu@users.noreply.github.com>
2022-01-05 00:06:50 -0300

    added menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { wallComponent } from './Components/wall/wall.component';
 import { homeComponent } from './Components/home/home.component';
 import { barComponent } from './Components/bar/bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './Components/menu/menu.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     wallComponent,
     homeComponent,
     barComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
